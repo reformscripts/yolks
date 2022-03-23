5f2303512cc689f7cfccafc7b736bfbd92d0948a
Reform Scripts <91909347+reformscripts@users.noreply.github.com>
2022-03-23 12:31:46 -0400

    Update entrypoint.sh

--- games/source/entrypoint.sh
+++ games/source/entrypoint.sh
@@ -70,11 +70,16 @@ if [ "${SRCDS_APPID}" == "4020" ] && [ ! -z ${GIT_USERNAME} ] && [ ! -z ${GIT_PE
     cd ./garrysmod || exit 1
     echo -e "Checking git version..."
     git --version
-    echo -e "Running git commands..."
+    echo -e "Initializing git..."
+    git init
+    echo -e "Adding remote to our git..."
     git remote add origin https://${GIT_USERNAME}:${GIT_PERSONAL_TOKEN}@${GIT_URL}
-    git add .
-    git stash
-    git pull
+    echo -e "Fetching git changes..."
+    git fetch -all
+    echo -e "Backing up local changes..."
+    git branch backup
+    echo -e "Pushing git changes to server..."
+    git reset --hard origin/main
     echo -e "Git has finished!"
     cd ../ || exit 1
     echo -e "Trying to boot the server now..."
